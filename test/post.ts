6d3b9415543448a96978dd0e7e1380c9e18c6b18
abotturi <60594258+abotturi@users.noreply.github.com>
2022-09-18 15:50:21 -0300

    Add post test

---
+++ test/post.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import { getAxios, postAxios } from './axios'
+
+describe("Post Route Test", () => {
+    describe('GET', () => {
+        it('return all posts', async () => {
+            expect(await getAxios('/post')).to.equal(200)
+        })
+    })
+
+    describe('POST', () => {
+        it('Add new post successfully', async () => {
+            expect(await postAxios('/post', {name: 'Fintipenn', text: 'Hello'})).to.equal(200)
+        })
+    })
+})
+
+
+import './comment'
