db5cc0cc8164600ff4bbb0f27d03f969851ddd14
abotturi <60594258+abotturi@users.noreply.github.com>
2022-09-18 15:48:51 -0300

    Add axios in test folder

---
+++ test/axios.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://127.0.0.1:8080'
+})
+
+export const getAxios = async (url:string = '/') => {
+    return await instance.get(url)
+    .then(res => {        
+        return res.status
+    })
+    .catch(e => {
+        console.log(e)
+    })
+}
+
+export const postAxios = async (url: string = '/', data: {}) => {
+    return await instance.post(url, data)
+    .then(res => {
+        return res.status
+    })
+    .catch(e => {
+        console.log(e)
+    })
+}
