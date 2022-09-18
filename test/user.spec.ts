091ad2aaa9a81f81bcac28a943a6c5536007831a
abotturi <60594258+abotturi@users.noreply.github.com>
2022-09-18 15:50:02 -0300

    Add user test

---
+++ test/user.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { getAxios, postAxios } from './axios'
+
+describe("User Route Test", () => {
+    describe('GET', () => {
+        it('return existing user', async () => {
+            expect(await getAxios('/user/Fintipenn')).to.equal(200)
+        })
+    })
+
+    describe('POST', () => {
+        it('Add new user successfully', async () => {
+            expect(await postAxios('/user', {name: 'Adandor'})).to.equal(200)
+        })
+    })
+})
+
+import './post'
